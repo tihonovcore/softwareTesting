7bfe30e927b2fb22a1b61b413f2a39b59c01ac7f
Vitaly <tihonovcore@gmail.com>
2020-10-21 22:07:03 +0300

    create clickable board

--- testing/client/src/App.js
+++ testing/client/src/App.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: null
+        };
+    }
+
+    render() {
+        return (
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
+            </button>
+        );
+    }
 }
 
-export default App;
+class Board extends React.Component {
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+
+    renderSquare(i) {
+        return <Square />;
+    }
+}
+
+export default Board;

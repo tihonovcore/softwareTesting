c2ed966da1e383055069343ff4fa624c0fcf473a
Vitaly <tihonovcore@gmail.com>
2020-10-23 17:47:25 +0300

    add frontend tests

---
+++ testing/client/src/test/Square.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Square from "../app/Square";
+
+let container = null;
+beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+it("renders with or without a value", () => {
+    act(() => {
+        render(<Square />, container);
+    });
+    expect(container.textContent).toBe("");
+
+    act(() => {
+        render(<Square value={"X"}/>, container);
+    });
+    expect(container.textContent).toBe("X");
+});
